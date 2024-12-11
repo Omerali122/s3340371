fe1e8aaef47a01290eb5b9984389dfaf79da7386
Omerali122 <S3340371@live.tees.ac.uk>
2024-12-11 10:00:00 +0530

    Jobs Details screen

--- app/src/main/java/uk/ac/tees/mad/jobs/ui/mainapp/HomeScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/jobs/ui/mainapp/HomeScreen.kt
@@ -1,18 +1,23 @@
 package uk.ac.tees.mad.jobs.ui.mainapp
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
@@ -50,9 +55,13 @@ fun JobDetails(
     modifier: Modifier = Modifier
 ) {
     Card(
+        modifier = modifier
+            .fillMaxWidth(),
         onClick = {
 
-        }
+        },
+        border = BorderStroke(1.dp, color = Color.Black),
+        elevation = CardDefaults.elevatedCardElevation(15.dp)
     ) {
         Text(
             text = job.title,
